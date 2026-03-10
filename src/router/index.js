1bf08985f03df7e790df390a4808483e23417241
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-10 11:07:26 +0200

    feat: add main application routes

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      route: 'homeRoute',
+      component: 'HomeView',
+    },
+
+    {
+      path: '/reservation',
+      route: 'reservationRoute',
+      component: 'ReservationView'
+    },
+  ],
 })
 
 export default router
