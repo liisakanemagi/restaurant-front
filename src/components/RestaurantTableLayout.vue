55b9f56ba27a6ba395743b40de2a135dda2d7426
liisakanemagi <liisa.kanemagi@gmail.com>
2026-03-11 13:45:34 +0200

    style: Add floor-plain-area style

--- src/components/RestaurantTableLayout.vue
+++ src/components/RestaurantTableLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="floor-plain-grid">
+  <div class="floor-plain-area">
 
 
 
@@ -15,3 +15,18 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.floor-plain-grid{
+  position: relative;
+  width: 100%;
+  height: 60px;
+  background-color: palegreen;
+  border: 2px solid #45804d;
+  border-radius: 15px;
+  overflow: hidden;
+  box-shadow: inset 0 2px 10px rgba(0,0,0,0.05);
+}
+
+
+</style>
